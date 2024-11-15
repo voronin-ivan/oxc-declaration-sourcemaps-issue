6ab8e6188da7e862d539590613e3756856d7b066
Ivan Voronin <ivan.v@miro.com>
2024-11-15 13:52:01 +0100

    add declaration files

---
+++ tsc/math.d.ts
@@ -0,0 +1,5 @@
+export declare const sum: (a: number, b: number) => number;
+export declare const subtract: (a: number, b: number) => number;
+export declare const multiply: (a: number, b: number) => number;
+export declare const divide: (a: number, b: number) => number;
+//# sourceMappingURL=math.d.ts.map
